ddfeeee57cb485850ba91be2a8c4ccde425ad3d7
youjeong2 <progress.uj11@gmail.com>
2020-11-09 17:01:40 +0900

    finish home

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Modal from "react-modal";
+// import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
+// serviceWorker.unregister();
